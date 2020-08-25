548ef755df1265bf3c6a9eeca527c57e3ce52920
Tasha Lane <designtmg77@gmail.com>
2020-08-25 14:20:27 -0500

    feeling the easy medium and hard

--- Program.cs
+++ Program.cs
@@ -14,13 +14,38 @@ namespace guessing_game
 
         {
 
-            int secretNumber = new Random().Next(1, 101);
+            //variable that holds the secretNumber
 
+            int secretNumber = new Random().Next(1, 101);
+            //Holds the number of Attempts
             int numberOfAttempts = 0;
+            int attemptsAvailable = 0;
+            bool difficultySet = false;
 
             Console.WriteLine($"Can you guess the secret number? >>");
+            while (!difficultySet)
+                Console.WriteLine("Select difficulty easy,medium, hard.");
+            string difficulty = Console.ReadLine();
+            {
+                if (difficulty == "easy")
+                {
+                    attemptsAvailable = 8;
+                    difficultySet = true;
 
-            while (numberOfAttempts < 4)
+                }
+                else if (difficulty == "medium")
+                {
+                    attemptsAvailable = 6;
+                    difficultySet = true;
+                }
+                else if (difficulty == "hard")
+                {
+                    attemptsAvailable = 4;
+                    difficultySet = true;
+                }
+            }
+
+            while (numberOfAttempts < attemptsAvailable)
             {
                 Console.WriteLine($"Your Attempts Remaining {4 - numberOfAttempts}");
 
