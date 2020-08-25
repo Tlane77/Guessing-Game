15c8c49be12d618b9ee99cf9583330a877b08a5d
Tasha Lane <designtmg77@gmail.com>
2020-08-25 10:39:21 -0500

    initial files

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessing_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        string message = "Do you want to close this window?";
+        string title = "Close Window";
+        MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+        DialogResult result = MessageBox.Show(message, title, buttons);
+        if (result == DialogResult.Yes)
+        {
+            this.Close();
+        }
+        else
+        {
+            // Do something  
+
+            {
+                Console.WriteLine("Guess the Secret Number");
+            }
+        }
